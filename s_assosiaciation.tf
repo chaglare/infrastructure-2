0a4080400c2d3a6aa435c9933ed59b141c93bbb1
Caglar Cavdar <chaglare@gmail.com>
2019-08-07 23:10:53 -0500

    edited

--- s_assosiaciation.tf
+++ s_assosiaciation.tf
@@ -1,7 +1,7 @@
 # resource "aws_route_table_association" "ad" {
 #   subnet_id      = "${aws_subnet.public-ad.id}"
 #   route_table_id = "${aws_route_table.r.id}"
-}
+#}
 
 resource "aws_route_table_association" "b" {
   subnet_id      = "${aws_subnet.public-b.id}"
