11daaf730fe80b0b68b183b7e5c227b646c2a2e6
Caglar Cavdar <chaglare@gmail.com>
2019-08-07 22:40:43 -0500

    edited

--- s_assosiaciation.tf
+++ s_assosiaciation.tf
@@ -1,17 +1,14 @@
 resource "aws_route_table_association" "a" {
   subnet_id      = "${aws_subnet.public-a.id}"
-  count          = "${var.count}"
   route_table_id = "${aws_route_table.r.id}"
 }
 
 resource "aws_route_table_association" "b" {
   subnet_id      = "${aws_subnet.public-b.id}"
-  count          = "${var.count}"
   route_table_id = "${aws_route_table.r.id}"
 }
 
 resource "aws_route_table_association" "c" {
   subnet_id      = "${aws_subnet.public-c.id}"
-  count          = "${var.count}"
   route_table_id = "${aws_route_table.r.id}"
 }
