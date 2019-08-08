4d65ff1b22bcfbdff53a30e973fa16e587895532
Caglar Cavdar <chaglare@gmail.com>
2019-08-07 23:09:46 -0500

    edited

--- s_assosiaciation.tf
+++ s_assosiaciation.tf
@@ -1,4 +1,14 @@
-resource "aws_route_table_association" "ad" {
-  subnet_id      = "${aws_subnet.public-ad.id}"
+# resource "aws_route_table_association" "ad" {
+#   subnet_id      = "${aws_subnet.public-ad.id}"
+#   route_table_id = "${aws_route_table.r.id}"
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = "${aws_subnet.public-b.id}"
   route_table_id = "${aws_route_table.r.id}"
 }
+
+# resource "aws_route_table_association" "ce" {
+#   subnet_id      = "${aws_subnet.public-ce.id}"
+#   route_table_id = "${aws_route_table.r.id}"
+# }
